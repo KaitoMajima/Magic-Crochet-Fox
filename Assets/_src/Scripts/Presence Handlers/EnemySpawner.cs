d54a07feb917b5b9318574dd3df7c165240ce272
JetStream Sam <dorado.451@hotmail.com>
2021-06-13 09:47:48 -0300

    Gameplay done

--- Assets/_src/Scripts/Presence Handlers/EnemySpawner.cs
+++ Assets/_src/Scripts/Presence Handlers/EnemySpawner.cs
@@ -39,5 +39,30 @@ namespace KaitoMajima
             Initialized = true;
             return enemies;
         }
+
+        public void NormalSpawn()
+        {
+            if(Initialized)
+                return;
+
+            int spawnAmount = Mathf.RoundToInt(UnityEngine.Random.Range(enemySpawnMinMax.x, enemySpawnMinMax.y));
+
+            if(spawnAmount > enemySpawnPoints.Count)
+                spawnAmount = enemySpawnPoints.Count;
+            
+            List<Transform> shuffledSpawnPoints = enemySpawnPoints;
+            RandomValues.Shuffle(ref shuffledSpawnPoints);
+
+            List<GameObject> enemies = new List<GameObject>();
+            int enemyCycle = 0;
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                var enemyObj = Instantiate(enemyPrefab[enemyCycle], shuffledSpawnPoints[i].position, Quaternion.identity);
+                enemyCycle += 1;
+                enemyCycle %= enemyPrefab.Count;
+                enemies.Add(enemyObj);
+            }
+            Initialized = true;
+        }
     }
 }
